eb5d021df8a115ef74bf08a3288c634bff852e96
이현승 <77336664+gesal03@users.noreply.github.com>
2024-05-22 22:00:26 +0900

    ✨ Add Annotation(EnableJpaAuditing)

--- yourside/src/main/java/com/likelion/yourside/YoursideApplication.java
+++ yourside/src/main/java/com/likelion/yourside/YoursideApplication.java
@@ -2,8 +2,10 @@ package com.likelion.yourside;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class YoursideApplication {
 
 	public static void main(String[] args) {
