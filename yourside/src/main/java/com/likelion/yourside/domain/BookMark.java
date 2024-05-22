fa6e422a957a38f1a11546175d0b813e38a937dd
이현승 <77336664+gesal03@users.noreply.github.com>
2024-05-22 23:20:28 +0900

    ✨ Add BookMark Entity

---
+++ yourside/src/main/java/com/likelion/yourside/domain/BookMark.java
@@ -0,0 +1,21 @@
+package com.likelion.yourside.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class BookMark {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+}
