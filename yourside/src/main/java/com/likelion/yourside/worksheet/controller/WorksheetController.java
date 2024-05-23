1aed5799fda7d6ea77544942c10f6c544e6b4f1e
이현승 <77336664+gesal03@users.noreply.github.com>
2024-05-24 03:50:26 +0900

    ✨ Add Controller register

--- yourside/src/main/java/com/likelion/yourside/worksheet/controller/WorksheetController.java
+++ yourside/src/main/java/com/likelion/yourside/worksheet/controller/WorksheetController.java
@@ -1,7 +1,10 @@
 package com.likelion.yourside.worksheet.controller;
 
+import com.likelion.yourside.util.response.CustomAPIResponse;
 import com.likelion.yourside.worksheet.service.WorksheetService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/worksheet")
 public class WorksheetController {
     private final WorksheetService worksheetService;
+
+    @PostMapping
+    public ResponseEntity<CustomAPIResponse<?>> register(WorksheetRegisterRequestDto worksheetRegisterRequestDto) {
+        return worksheetService.register(worksheetRegisterRequestDto);
+    }
 }
