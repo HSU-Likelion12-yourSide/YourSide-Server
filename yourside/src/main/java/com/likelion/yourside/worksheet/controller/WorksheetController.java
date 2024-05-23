d15057c0d3e8b76ae03fe3452fbcba16be4ccf3e
이현승 <77336664+gesal03@users.noreply.github.com>
2024-05-24 03:31:17 +0900

    ✨ Add WorksheetController

---
+++ yourside/src/main/java/com/likelion/yourside/worksheet/controller/WorksheetController.java
@@ -0,0 +1,12 @@
+package com.likelion.yourside.worksheet.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/worksheet")
+public class WorksheetController {
+    private final WorksheetService worksheetService;
+}
