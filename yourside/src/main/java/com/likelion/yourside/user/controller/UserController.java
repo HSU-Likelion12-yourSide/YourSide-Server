9d39e2c8e92259ec48c703fb754373b82b883e61
이현승 <77336664+gesal03@users.noreply.github.com>
2024-05-23 19:20:31 +0900

    ✨ Add signUp Controller

--- yourside/src/main/java/com/likelion/yourside/user/controller/UserController.java
+++ yourside/src/main/java/com/likelion/yourside/user/controller/UserController.java
@@ -1,13 +1,24 @@
 package com.likelion.yourside.user.controller;
 
 import com.likelion.yourside.user.service.UserService;
+import com.likelion.yourside.util.response.CustomAPIResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("api/user/")
+@RequestMapping("api/user")
 public class UserController {
     private final UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<CustomAPIResponse<?>> signUp(@RequestBody @Valid UserSignUpDto userSignUpDto) {
+        return userService.signUp(userSignUpDto);
+    }
+
 }
