a1b86b29b09f4f8365aac87f06cca8c0036836d7
이현승 <77336664+gesal03@users.noreply.github.com>
2024-05-24 16:05:31 +0900

    ✏️ Fix

--- yourside/src/main/java/com/likelion/yourside/worksheet/dto/WorksheetRegisterRequestDto.java
+++ yourside/src/main/java/com/likelion/yourside/worksheet/dto/WorksheetRegisterRequestDto.java
@@ -1,5 +1,6 @@
 package com.likelion.yourside.worksheet.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,11 +12,18 @@ import lombok.Setter;
 public class WorksheetRegisterRequestDto {
     private String title;
     private String content;
+    @JsonProperty("total_pay")
     private int totalPay;
+    @JsonProperty("extra_pay")
     private boolean extraPay;    // 가산 수당
+    @JsonProperty("week_pay")
     private boolean weekPay;     // 주휴 수당
+    @JsonProperty("night_pay")
     private boolean nightPay;    // 야간 수당
+    @JsonProperty("overtime_pay")
     private boolean overtimePay; // 연장 근로 수당
+    @JsonProperty("holiday_pay")
     private boolean holidayPay;  // 휴일 근로 수당
+    @JsonProperty("user_id")
     private Long userId;
 }
