6e7149b09abacc731d6ee686e64a866554bd79dc
khyaejin <olivia019182@naver.com>
2024-05-25 09:30:20 +0900

    ✨ Comment 관련 기본 세팅

---
+++ yourside/src/main/java/com/likelion/yourside/comment/dto/CommentCreateDto.java
@@ -0,0 +1,12 @@
+package com.likelion.yourside.comment.dto;
+
+import com.likelion.yourside.domain.Comment;
+import com.likelion.yourside.domain.Posting;
+import com.likelion.yourside.domain.User;
+import com.likelion.yourside.user.repository.UserRepository;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+public class CommentCreateDto {
+
+}
